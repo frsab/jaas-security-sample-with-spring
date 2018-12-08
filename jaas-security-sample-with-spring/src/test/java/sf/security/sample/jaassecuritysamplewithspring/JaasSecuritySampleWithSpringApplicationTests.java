28a1f127613fce4b431a7b884c71401c68183c03
Saber FRAJ <frejsaber@gmail.com>
2018-12-08 16:24:33 +0100

    first commit

---
+++ jaas-security-sample-with-spring/src/test/java/sf/security/sample/jaassecuritysamplewithspring/JaasSecuritySampleWithSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package sf.security.sample.jaassecuritysamplewithspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaasSecuritySampleWithSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/main/java/java/sf/security/sample/jaassecuritysamplewithspring/JaasSecuritySampleWithSpringApplication.java
@@ -0,0 +1,12 @@
+package java.sf.security.sample.jaassecuritysamplewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaasSecuritySampleWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaasSecuritySampleWithSpringApplication.class, args);
+	}
+}
